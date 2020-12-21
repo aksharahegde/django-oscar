84049482f276321b0e157f9ed984c192233d8ee6
Akshara Hegde <akshara.dt@gmail.com>
2020-12-21 17:28:47 +0530

    Removed unused import

--- src/oscar/apps/offer/apps.py
+++ src/oscar/apps/offer/apps.py
@@ -15,12 +15,12 @@ class OfferConfig(OscarConfig):
     def ready(self):
         from . import signals  # noqa
 
-        self.detail_view = get_class('offer.views', 'OfferDetailView')
-        self.list_view = get_class('offer.views', 'OfferListView')
+        # self.detail_view = get_class('offer.views', 'OfferDetailView')
+        # self.list_view = get_class('offer.views', 'OfferListView')
 
     def get_urls(self):
         urls = [
-            path('', self.list_view.as_view(), name='list'),
-            path('<slug:slug>/', self.detail_view.as_view(), name='detail'),
+            # path('', self.list_view.as_view(), name='list'),
+            # path('<slug:slug>/', self.detail_view.as_view(), name='detail'),
         ]
         return self.post_process_urls(urls)
